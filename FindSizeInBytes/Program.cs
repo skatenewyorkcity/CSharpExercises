d7db0e85a0fa1b12f1657ef051bcec4d3c23891f
Garboza <p.garboza@gmail.com>
2017-10-27 10:12:04 -0400

    Updated Repo

---
+++ FindSizeInBytes/Program.cs
@@ -0,0 +1,27 @@
+﻿//29. Write a C# program to find the size of a specified file in bytes.
+//Sample Output:
+//Size of a file: 31 
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindSizeInBytes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileInfo f = new FileInfo("/home/students/abc.txt");
+            Console.WriteLine("\nSize of file: " + f.Length.ToString());
+
+            System.Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ ReverseWordsInSentence/Program.cs
@@ -0,0 +1,42 @@
+﻿//28. Write a C# program to reverse the words of a sentence.
+//Sample Output:
+//Original String: Display the pattern like pyramid using the alphabet.
+//Reverse String: alphabet.the using pyramid like pattern the Display
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWordsInSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string line = "Display the pattern like pyramid using the alphabet.";
+            Console.WriteLine("\nOriginal String: " + line);
+            string result = "";
+            List<string> wordsList = new List<string>();
+
+            string[] words = line.Split(new[]
+            {
+                " "
+            }, StringSplitOptions.None);
+            for(int i = words.Length - 1; i >= 0; i--)
+            {
+                result += words[i] + " ";
+            }
+
+            wordsList.Add(result);
+            foreach(String s in wordsList)
+            {
+                Console.WriteLine("\nReverse String: " + s);
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
